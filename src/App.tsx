c4ecbc46ff19240b895ec20108142d208c34a06d
yukicoder0509 <bendoris0509@gmail.com>
2025-08-12 17:08:54 +0800

    setup react router, example page,  path alias to

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router";
+import Home from "@/pages/Home.tsx";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-2xl font-bold">Hello World</h1>;
+}
